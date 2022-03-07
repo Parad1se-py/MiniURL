de06d28a12376435508f7addee36c8dbcff30f3f
Arjun Ladha <tigerninja2234@gmail.com>
2022-03-07 16:06:32 +0530

    🚮 Clear decorators.py for now

--- utility/decorators.py
+++
@@ -1,6 +0,0 @@
-from flask import session, abort
-
-def login_is_required(function):
-    def wrapper(*args, **kwargs):
-        return abort(401) if "email" not in session else function
-    return wrapper
